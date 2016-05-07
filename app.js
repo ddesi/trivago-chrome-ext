249515347b4b78a3b76cc416481f0e4bdc6becd7
desi <desi.dmt@gmail.com>
2016-05-07 22:54:29 +0200

    commented code

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// this is why i decided to go with: i simply eliminated a bunch of elements from the page to make it
+// look a little less distracting for the user. the background color also changes on scrolldown
+
 alert('Hello! This is the new layout!');
 
 // make logo space and navigation smaller
-
 $('.siteheader').css({"height":"36px"});
 $('.siteheader__logo-link').css({"padding":"0"});
 $('.hero__content').css({"background-color":"#fff"});
 
 // for each hotel show only main image, hotel name, rating, main price, distance
-// hide other elements
+// hide other elements, remove sidebar and item control bar
 
 $('#js_itemlist_controls').hide();
 $('.nav').remove();
@@ -15,21 +17,13 @@ $('.nav').remove();
 $('#main_content').css({"background-color": "transparent"});
 $('.centerwrapper').removeClass('centerwrapper centerwrapper--item clearfix').addClass('newCenter');
 
-// $('.item__image-gallery-entry').click(function(){
-// 	$('.item__slideout').fadeIn('slow');
-// })
-
-// $('#js_trv_map_container').removeClass('is-hidden').addClass('newMap');
-
-// $('#js_itemlist').removeClass('item-order itemlist hotellist').addClass('newItem');
-
+// change bg color
 $('.js-main').removeClass('js-main main-wrap').addClass('newItem');
 
 $('.container_itemlist').css({"background-color":"transparent"});
 $('.centercol').css({"background-color":"transparent"});
 
-// $('#js_itemlist > li').removeClass('hotel').addClass('newHotel');
-
+// remove little elements in the item details
 $('.bookmark').remove();
 $('.item__mini-icon').remove();
 $('.review__count').remove();
@@ -37,16 +31,16 @@ $('.item__deal-other').remove();
 $('.item__flag').remove();
 $('.btn').removeClass('icon-icn_arrow_deal');
 
+// do not show footer
 $('footer').remove();
 
 
 
 // change background color on scrolldown
-
 $(document).ready(function(){ 
 
 	var scroll_position = 0,
-    animation_start_pos = 0,
+    animation_start_position = 0,
     animation_end_position = 1000,
     // trivago colors ;)
     beginning_color = new $.Color('rgb(0,127,173)'), 
@@ -54,9 +48,9 @@ $(document).ready(function(){
     $(document).scroll(function() {
         scroll_position = $(this).scrollTop(); 
 
-        if(scroll_position >= animation_start_pos && scroll_position <= animation_end_position ) { 
+        if(scroll_position >= animation_start_position && scroll_position <= animation_end_position ) { 
 
-        	var percentScrolled = scroll_position / (animation_end_position - animation_start_pos),
+        	var percentScrolled = scroll_position / (animation_end_position - animation_start_position),
             newR = beginning_color.red() + ((ending_color.red() - beginning_color.red()) * percentScrolled),
             newG = beginning_color.green() + ((ending_color.green() - beginning_color.green()) * percentScrolled),
             newB = beginning_color.blue() + ((ending_color.blue() - beginning_color.blue()) * percentScrolled),
@@ -67,11 +61,53 @@ $(document).ready(function(){
             $('.newItem').animate({backgroundColor: newColor}, 0);
         } else if (scroll_position > animation_end_position) {
              $('.newItem').animate({backgroundColor: ending_color}, 0);
-        } else if (scroll_position < animation_start_pos) {
+        } else if (scroll_position < animation_start_position) {
              $('.newItem').animate({backgroundColor: beginning_color}, 0);
         } else { }
     });
 });
+
+
+
+// a couple more ideas included:
+// display the map on the side with the item list, but then i figured it could look too overwhelming
+// (also i could not get the map to load properly)
+// $('#js_trv_map_container').removeClass('is-hidden').addClass('newMap');
+
+// i also thought about displaying a 'tinder' version of the results, i.e. show one hotel at a time
+// and then show the next one on swipe (well with left and right keys probably)
+// i dont think it would have looked that great from a UX POV but this is the code i started to write:
+
+// var arr = new Array(),
+// i = 0,
+// current = $('#js_itemlist').find('li:first'),
+// arr = $('#js_itemlist > li');
+
+// something like: 
+
+// next_hotel = function(){ 
+//          if (i < arr.length - 1){
+//             i +=1;
+//             $(current).hide();
+//             current = $(current).next('li').slideToggle();
+//          } else {
+//             i = 0;
+//             $(current).hide();
+//             current = $('#js_itemlist').find('li:first').slideToggle();
+//          };
+
+// previous_hotel = function(){ 
+//          if(i > 0 ){
+//             i -= 1;
+//             $(current).hide();
+//             current = $(current).prev('li').slideToggle();
+//          } else {
+//             i = arr.length - 1;
+//             $(current).hide();
+//             current = $('#js_itemlist').find('li:nth-child(numli)').slideToggle();
+//          };
+
+// i would remove all classes and create my own to display only the current item
 
 
 
