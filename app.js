ddd03177ab6efda3338ca7357bab7760e0575e37
desi <desi.dmt@gmail.com>
2016-05-07 18:12:47 +0200

    reviewd js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 alert('Hello! This is the new layout!');
 
-var map = $('#js_bigMap');
-
-// IDEA 0
-// show 5 items like dribbble layout, mobile style, hide the rest. Show 5 more on click
-
+// make logo space and navigation smaller
 
 $('.siteheader').css({"height":"36px"});
 $('.siteheader__logo-link').css({"padding":"0"});
 $('.hero__content').css({"background-color":"#fff"});
 
+// for each hotel show only main image, hotel name, rating, main price, distance
+// hide other elements
+
 $('#js_itemlist_controls').hide();
-// $('#js_leftcol').hide();
 $('.nav').remove();
 
 $('#main_content').css({"background-color": "transparent"});
@@ -41,61 +39,40 @@ $('.btn').removeClass('icon-icn_arrow_deal');
 
 $('footer').remove();
 
-// function minimal(){
-// 	$('#js_itemlist .hotel').each(function(i){
-// 		var item = $(this),
-// 		img = item.find('.item_image img').attr('src'),
-// 		igInnerHtml += 
-// 		'<div class="newItem"><div class="image">' +
-// 		'<h2>' + item__name__copytext + '</h2>' +
-// 		'<span>' + name__star + '</span>' +
-//         '<a target="_blank" href="'+data.link+'"><img src="'+img+'"></a>' + 
-//         '</div></div>'
-//     });
 
 
-
-
-
-
-
-
-
-
+// change background color on scrolldown
 
 $(document).ready(function(){ 
-//using color plugin
 
-	var scroll_pos = 0,
-    animation_begin_pos = 0,
-    animation_end_pos = 1000,
+	var scroll_position = 0,
+    animation_start_pos = 0,
+    animation_end_position = 1000,
+    // trivago colors ;)
     beginning_color = new $.Color('rgb(0,127,173)'), 
     ending_color = new $.Color('rgb(201,74,56)');
     $(document).scroll(function() {
-        scroll_pos = $(this).scrollTop(); 
+        scroll_position = $(this).scrollTop(); 
 
-        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) { 
+        if(scroll_position >= animation_start_pos && scroll_position <= animation_end_position ) { 
 
-        	var percentScrolled = scroll_pos / (animation_end_pos - animation_begin_pos),
+        	var percentScrolled = scroll_position / (animation_end_position - animation_start_pos),
             newR = beginning_color.red() + ((ending_color.red() - beginning_color.red()) * percentScrolled),
             newG = beginning_color.green() + ((ending_color.green() - beginning_color.green()) * percentScrolled),
             newB = beginning_color.blue() + ((ending_color.blue() - beginning_color.blue()) * percentScrolled),
             newColor = new $.Color(newR, newG, newB);
+            // or alternatively
             // var newColor = new $.Color('rgb(244, 143, 0)');
             
-            $('.newItem').animate({ backgroundColor: newColor }, 0);
-        } else if ( scroll_pos > animation_end_pos ) {
-             $('.newItem').animate({ backgroundColor: ending_color }, 0);
-        } else if ( scroll_pos < animation_begin_pos ) {
-             $('.newItem').animate({ backgroundColor: beginning_color }, 0);
+            $('.newItem').animate({backgroundColor: newColor}, 0);
+        } else if (scroll_position > animation_end_position) {
+             $('.newItem').animate({backgroundColor: ending_color}, 0);
+        } else if (scroll_position < animation_start_pos) {
+             $('.newItem').animate({backgroundColor: beginning_color}, 0);
         } else { }
     });
 });
 
-
-
-
-// for each hotel show only main image, hotel name, rating, main price, maaaaaybe distance
 
 
 
